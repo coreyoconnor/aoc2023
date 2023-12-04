99648034ba026e88b85328fcf3eda11537e7fed7
Corey O'Connor <coreyoconnor@gmail.com>
2023-12-04 17:48:46 +0800

    ~

---
+++ day-1-part-1.scala
@@ -0,0 +1,23 @@
+
+val FirstCalibrationValue = raw"^[^\d]*(\d).*$$".r
+
+val SecondCalibrationValue = raw"^.*(\d)[^\d]*$$".r
+
+@main
+def main(): Unit = {
+  val input = scala.io.Source.fromFile("day-1-input.txt", "UTF-8").getLines().filterNot(_.isBlank).toSeq
+
+  val firstDigits = input.map {
+    case FirstCalibrationValue(a) => a
+  }
+
+  val secondDigits = input.map {
+    case SecondCalibrationValue(b) => b
+  }
+
+  val calibrationValues = firstDigits.zip(secondDigits).map {
+    case (a, b) => s"$a$b".toInt
+  }
+  println(calibrationValues.sum)
+}
+
