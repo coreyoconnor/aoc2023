32a9098ed6a181b4e07b12fd565142c3fe0e16bf
Corey O'Connor <coreyoconnor@gmail.com>
2023-12-05 09:12:05 +0800

    day 1

--- day-1-part-1.scala
+++ day-1-part-1.scala
@@ -1,23 +1,22 @@
+object Day1Part1 {
+  val FirstCalibrationValue = raw"^[^\d]*(\d).*$$".r
 
-val FirstCalibrationValue = raw"^[^\d]*(\d).*$$".r
+  val SecondCalibrationValue = raw"^.*(\d)[^\d]*$$".r
 
-val SecondCalibrationValue = raw"^.*(\d)[^\d]*$$".r
+  def main(args: Array[String]): Unit = {
+    val input = scala.io.Source.fromFile("day-1-input.txt", "UTF-8").getLines().filterNot(_.isBlank).toSeq
 
-@main
-def main(): Unit = {
-  val input = scala.io.Source.fromFile("day-1-input.txt", "UTF-8").getLines().filterNot(_.isBlank).toSeq
+    val firstDigits = input.map {
+      case FirstCalibrationValue(a) => a
+    }
 
-  val firstDigits = input.map {
-    case FirstCalibrationValue(a) => a
+    val secondDigits = input.map {
+      case SecondCalibrationValue(b) => b
+    }
+
+    val calibrationValues = firstDigits.zip(secondDigits).map {
+      case (a, b) => s"$a$b".toInt
+    }
+    println(calibrationValues.sum)
   }
-
-  val secondDigits = input.map {
-    case SecondCalibrationValue(b) => b
-  }
-
-  val calibrationValues = firstDigits.zip(secondDigits).map {
-    case (a, b) => s"$a$b".toInt
-  }
-  println(calibrationValues.sum)
 }
-
--- day-1-part-2.scala
+++ day-1-part-2.scala
@@ -1,23 +1,34 @@
+object Day1Part2 {
+  val digitWords = Seq(
+    "one" -> 1,
+    "two" -> 2,
+    "three" -> 3,
+    "four" -> 4,
+    "five" -> 5,
+    "six" -> 6,
+    "seven" -> 7,
+    "eight" -> 8,
+    "nine" -> 9
+  )
 
-val FirstCalibrationValue = raw"^[^\d]*(\d).*$$".r
+  val toDigit = digitWords.toMap ++ digitWords.map(_._2).map(d => d.toString -> d)
+  val digitMatch = toDigit.keys.mkString("|").r
 
-val SecondCalibrationValue = raw"^.*(\d)[^\d]*$$".r
+  def main(args: Array[String]): Unit = {
+    val input = scala.io.Source.fromFile("day-1-input.txt", "UTF-8").getLines().filterNot(_.isBlank).toSeq
+    def allMatches(remaining: String): Seq[String] =
+      digitMatch.findFirstMatchIn(remaining) match {
+        case None => Seq.empty
+        case Some(value) => value.matched +: allMatches(remaining.drop(value.start + 1))
+      }
 
-@main
-def main(): Unit = {
-  val input = scala.io.Source.fromFile("day-1-input.txt", "UTF-8").getLines().filterNot(_.isBlank).toSeq
+    val values = input.map { line =>
+      val matches = allMatches(line)
+      toDigit(matches.head) * 10 + toDigit(matches.last)
+    }
+    println(values.mkString("\n"))
 
-  val firstDigits = input.map {
-    case FirstCalibrationValue(a) => a
+    println(values.sum)
   }
-
-  val secondDigits = input.map {
-    case SecondCalibrationValue(b) => b
-  }
-
-  val calibrationValues = firstDigits.zip(secondDigits).map {
-    case (a, b) => s"$a$b".toInt
-  }
-  println(calibrationValues.sum)
 }
 
