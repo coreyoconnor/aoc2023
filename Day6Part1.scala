80ba344ba268e0df493f9fef76b5b225ac8f02d9
Corey O'Connor <coreyoconnor@gmail.com>
2023-12-11 15:40:42 -0800

    Day 6 part 1

---
+++ Day6Part1.scala
@@ -0,0 +1,16 @@
+object Day6Part1 {
+  /* (total - pressed) * pressed
+   * EG:
+   * total = 7
+   * pressed = 3
+   * distance = 4 * 3 = 12
+   *
+   * total * pressed - pressed^2 - distnace = 0
+   * 7 * pressed - pressed^2 - 10 = 0
+   * (t*x + u)(v*x + w) = 0
+   * t*v*x^2 + (w*t + u*v)x + u*w = 0
+   * a*x^2 + b*x + c = 0
+   * b*x + c = -a*x^2
+   * b + c/x = -a*x
+   */
+}
